a508fe8137fbbef75c77f3c28ff5902299b7d838
Oussama-harbali <harbali.oussama@gmail.com>
2026-02-04 13:22:25 +0100

    Dockerize Spring Boot app

--- src/main/java/com/example/spring_github/SpringGithubApplication.java
+++ src/main/java/com/example/spring_github/SpringGithubApplication.java
@@ -2,12 +2,20 @@ package com.example.spring_github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringGithubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringGithubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGithubApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
+
