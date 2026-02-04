9f8a7c727a77b3d2e0422b6e3933122bc12a94fe
Oussama-harbali <harbali.oussama@gmail.com>
2026-02-04 11:19:49 +0100

    initial commit - Spring Boot Maven project

---
+++ src/main/java/com/example/spring_github/SpringGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_github/SpringGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
